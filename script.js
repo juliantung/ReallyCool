a9175d4a096625b88b45029606f7ffbeaaa46bf0
juliantung <156071429+juliantung@users.noreply.github.com>
2024-06-19 21:38:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    // Form animation
+    $(".form").on('click', function(){
+        $(this).addClass('active');
+    });
+
+    $(".submit").on('click', function() {
+        $(this).parent().parent().hide(300);
+        $(".ok_message").addClass("active");
+        $('#pageTitle').text("Feedback Received!");
+        $('#testing').show();
+    });
+
+    // Star rating functionality
+    let rating = 0;
+
+    window.highlightStars = function(num) {
+        for (let i = 1; i <= num; i++) {
+            $(`#ratings .star:nth-child(${i})`).addClass('highlighted');
+        }
+    };
+
+    window.resetStars = function() {
+        $('#ratings .star').removeClass('highlighted');
+    };
+
+    window.rate = function(num) {
+        rating = num;
+        resetStars();
+        highlightStars(num);
+        var pageTitle = document.getElementById('pageTitle');
+        if (num <= 3) {
+            pageTitle.textContent = "Tell us more";
+            $('#ratings').hide();
+            $('#testing').hide();
+            $('#feedback').show().addClass('active');
+        } else {
+            window.open('https://g.page/r/CZr2ArVSDCRuEBM/review');
+            //Paste review link above
+            window.location.href = 'https://kyson.sg/collections/all';
+            //Paste navigator link above such as website
+            //Change for specific companies
+        }
+    };
+
+    // Initially hide the feedback form
+    $('#feedback').hide();
+});
+
